28de7616674cd86f4102d3d06978a6535cd1287c
MarskTM <manhtokim@gmail.com>
2024-10-16 16:56:39 +0700

    init services interface

---
+++ baselib/server/running.go
@@ -0,0 +1,62 @@
+package server
+
+import (
+	"flag"
+	"math/rand"
+	"os"
+	"os/signal"
+	"runtime"
+	"runtime/pprof"
+	"syscall"
+	"time"
+
+	"github.com/golang/glog"
+)
+
+var enablePProf bool
+
+func init() {
+	flag.Set("alsologtostderr", "true")
+	flag.Set("log_dir", "/data/logKingtalk")
+	flag.Set("v", "1")
+	enablePProf = *flag.Bool("pprof_enable", true, "Enable profiling")
+	flag.Parse()
+}
+
+// ------------------------------------------------------------------------------------------------
+type Services interface {
+	Install() error
+	Start()
+	Shutdown(signals chan os.Signal)
+}
+
+func RunAppService(instance Services) {
+	defer glog.Flush()
+
+	// 1. Setup the runtime CPU
+	if runtime.NumCPU() > 1 {
+		runtime.GOMAXPROCS(runtime.NumCPU() - 1)
+	}
+
+	// 2. Seed the time to random
+	rand.Seed(time.Now().UnixNano())
+
+	// 3. Install the instance
+	if err := instance.Install(); err != nil {
+		glog.Fatalf("Error installing instance: %v", err)
+	}
+
+	// 4. Run the instance's loop
+	go instance.Start()
+
+	// 5. Handle signals for graceful shutdown
+	signals := make(chan os.Signal, 1)
+	signal.Notify(signals, syscall.SIGINT, syscall.SIGTERM)
+
+	instance.Shutdown(signals)
+
+	// 6. Wait for the signal to shutdown
+	if enablePProf {
+		pprof.StopCPUProfile()
+	}
+}
--- go.mod
+++ go.mod
@@ -2,6 +2,4 @@ module github.com/MarskTM/financial_report_server
 
 go 1.21.5
 
-require k8s.io/klog/v2 v2.130.1
-
-require github.com/go-logr/logr v1.4.1 // indirect
+require github.com/golang/glog v1.2.2
